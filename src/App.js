cca08a9e2bdba3511d74631987c3a72eaba04a88
Michael Williams <manicdesigner@gmail.com>
2019-08-27 16:35:18 -0400

    add useEffectfor setting state slice, title, and comments for thoughts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { GhibliFilmsApi } from './components/GhibliApis';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
+
 function App() {
+  const [films, setFilms] = useState();
+  useEffect(() => {
+    GhibliFilmsApi(setFilms);
+  }, []); 
+  console.log(films);
   return (
     <div className="App">
-      
+      <h1>Ghibli Films!</h1>
     </div>
   );
 }
