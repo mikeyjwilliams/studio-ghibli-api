2660f89a68c142b748419201e4af0115a109f1a6
Michael Williams <manicdesigner@gmail.com>
2019-08-27 18:23:50 -0400

    fix typo with description

--- src/components/FilmsComponents/Description.js
+++ src/components/FilmsComponents/Description.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Dimmer, Loader, Segment } from 'semantic-ui-react';
 
 export function Description(props) {
-    if(!props.director) return (
+    if(!props.films.description) return (
         <Segment>
             <Dimmer active>
                 <Loader inverted>Loading...</Loader>
             </Dimmer>
         </Segment>
     )
-    const { director } = props.director;
+    const { description } = props.films.description;
     return (
        <>
-        <p>{director}</p>
+        <p>{description}</p>
        </> 
     );
 }
