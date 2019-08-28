b324c81bb4bac141857de282e3fc07fd092470ae
Michael Williams <manicdesigner@gmail.com>
2019-08-28 09:32:24 -0400

    working with semantic-ui

---
+++ src/components/Films.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Dimmer, Loader, Segment, Card } from 'semantic-ui-react';
+import { Title } from './FilmsComponents/Title';
+import { Director } from './FilmsComponents/Director';
+import { ReleaseDate } from './FilmsComponents/ReleaseDate';
+import { RateScore } from './FilmsComponents/RateScore';
+import { Description } from './FilmsComponents/Description';
+
+export function Films(props) {
+    if (!props.films) return (
+        <Segment>
+            <Dimmer active>
+                <Loader inverted>Loading...</Loader>
+            </Dimmer>
+        </Segment>
+    )
+    const { films } = props;
+   
+    return (
+        <div>
+            {films.map((film) => {
+                return (
+
+                <Card key={film.id}
+                header= 'Studio Ghibli'
+                    meta= <ReleaseDate film={film} key={film.id + 'r'} />  
+                description =<Description film={film} key={film.id + 'd'} />
+                />
+            )})
+            }
+                
+                
+        </div>
+    )
+}
+
+// card component holding.
+  // title
+  // director
+  // release_date
+  // rt_score
+  // description
