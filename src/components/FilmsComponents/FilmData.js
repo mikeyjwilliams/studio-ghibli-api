06b17f223adf2e27040413a7ec73e9d8a272da88
Michael Williams <manicdesigner@gmail.com>
2019-09-23 13:04:07 -0400

    completed card now working on changing styles.

--- src/components/FilmsComponents/FilmData.js
+++ src/components/FilmsComponents/FilmData.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Icon } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 function FilmData() {
   // const { title, director, release_date, description, rt_score } = props;
   return (
     <Card>
-      <h2>Castle in the sky</h2>
-      <div>
-        <p>Directed by: Hayo</p>
-        <p>Released: 1986</p>
-      </div>
-      <p>description....</p>
-      <footer>score: 95</footer>
+      <Card.Content>
+        <Card.Header>Castle in the sky</Card.Header>
+        <Card.Meta>
+          <p>Directed by: Hayo</p>
+          <p>Released: 1986</p>
+        </Card.Meta>
+        <Card.Description>description....</Card.Description>
+        <Card.Content extra>
+          <Icon name='checkmark' /> score: 95
+        </Card.Content>
+      </Card.Content>
     </Card>
   );
 }
