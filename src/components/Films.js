1de3f32cad098f37de2cf7faae59da167716d0c0
Mikey Williams <manicdesigner@gmail.com>
2019-11-16 14:00:50 -0500

    film data shown in pargraphs

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,26 +1,33 @@
 import React from 'react';
 
 export function Films(props) {
-  if (!props.films) return <h2>Loading....</h2>;
-  const { films } = props;
+	if (!props.films) return <h2>Loading....</h2>;
+	const { films } = props;
 
-  return (
-    <div>
-      <div>
-        {films.map(film => {
-          return (
-            <div key={film.id}>
-              {/* <Title film={film} key={film.id + 't'} /> */}
-              <h2>{film.title}</h2>
-              {/* <ReleaseDate film={film} key={film.id + 'r'} />   */}
-              {/* <Description film={film} key={film.id + 'd'} /> */}
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				{films.map((film) => {
+					return (
+						<div key={film.id}>
+							{/* <Title film={film} key={film.id + 't'} /> */}
+							<h2>{film.title}</h2>
+							<div>
+								<p>Release date: {film.release_date}</p>
+								<p>Director: {film.director}</p>
+							</div>
+							<p>{film.description}</p>
+							<p>Film score: {film.rt_score}</p>
+
+							{/* <Description film={film} key={film.id + 'd'} /> */}
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
+export default Films;
 
 // card component holding.
 // title
